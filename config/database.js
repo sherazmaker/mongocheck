79fa6a65b18d5ffa6f183f42edcc995441bbeeb2
sherazmaker <sheraz.q@hashmakersol.net>
2022-06-20 15:33:35 +0500

    place recommanded settings

--- config/database.js
+++ config/database.js
@@ -14,17 +14,59 @@
 //   },
 // });
 
+// module.exports = ({ env }) => ({
+//   defaultConnection: "default",
+//   connections: {
+//     default: {
+//       connector: "mongoose",
+//       settings: {
+//         uri: env("DATABASE_URI"),
+//       },
+//       options: {
+//         ssl: true,
+//       },
+//     },
+//   },
+// });
+
 module.exports = ({ env }) => ({
   defaultConnection: "default",
   connections: {
     default: {
       connector: "mongoose",
       settings: {
-        uri: env("DATABASE_URI"),
+        client: "mongo",
+        host: env("DATABASE_HOST", "localhost"),
+        port: env.int("DATABASE_PORT", 27017),
+        database: env("DATABASE_NAME", "logomoto"),
+        username: env("DATABASE_USERNAME", "logomoto"),
+        password: env("DATABASE_PASSWORD", "logomoto"),
       },
       options: {
-        ssl: true,
+        authenticationDatabase: env("AUTHENTICATION_DATABASE"),
+        ssl: env("DATABASE_SSL"),
       },
     },
   },
 });
+
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'mongoose',
+//       settings: {
+//         client: 'mongo',
+//         host: env('DATABASE_HOST', 'localhost'),
+//         port: env.int('DATABASE_PORT', 27017),
+//         database: env('DATABASE_NAME', 'strapi'),
+//         username: env('DATABASE_USERNAME', 'strapi'),
+//         password: env('DATABASE_PASSWORD', 'strapi'),
+//       },
+//       options: {
+//         authenticationDatabase: env('AUTHENTICATION_DATABASE'),
+//         ssl: env('DATABASE_SSL'),
+//       },
+//     },
+//   },
+// });
