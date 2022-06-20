91399c1e8f756c05687919d3525ab13f2b276453
sherazmaker <sheraz.q@hashmakersol.net>
2022-06-20 15:44:29 +0500

    new check

--- config/database.js
+++ config/database.js
@@ -36,37 +36,11 @@ module.exports = ({ env }) => ({
       connector: "mongoose",
       settings: {
         client: "mongo",
-        host: env("DATABASE_HOST", "localhost"),
-        port: env.int("DATABASE_PORT", 27017),
-        database: env("DATABASE_NAME", "logomoto"),
-        username: env("DATABASE_USERNAME", "logomoto"),
-        password: env("DATABASE_PASSWORD", "logomoto"),
+        uri: "mongodb+srv://logomoto:logomoto@cluster0.cs9j1.mongodb.net/?retryWrites=true&w=majority",
       },
       options: {
-        authenticationDatabase: env("AUTHENTICATION_DATABASE"),
-        ssl: env("DATABASE_SSL"),
+        ssl: true,
       },
     },
   },
 });
-
-// module.exports = ({ env }) => ({
-//   defaultConnection: 'default',
-//   connections: {
-//     default: {
-//       connector: 'mongoose',
-//       settings: {
-//         client: 'mongo',
-//         host: env('DATABASE_HOST', 'localhost'),
-//         port: env.int('DATABASE_PORT', 27017),
-//         database: env('DATABASE_NAME', 'strapi'),
-//         username: env('DATABASE_USERNAME', 'strapi'),
-//         password: env('DATABASE_PASSWORD', 'strapi'),
-//       },
-//       options: {
-//         authenticationDatabase: env('AUTHENTICATION_DATABASE'),
-//         ssl: env('DATABASE_SSL'),
-//       },
-//     },
-//   },
-// });
